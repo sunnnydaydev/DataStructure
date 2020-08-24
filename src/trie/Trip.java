f08dc79ba186ad82311762bf10be695d8ebc09d7
Administrator <1246771571@qq.com>
2020-08-24 21:37:27 +0800

    add Trie base

--- src/trie/Trip.java
+++ src/trie/Trip.java
@@ -6,7 +6,6 @@ import java.util.TreeMap;
  * Create by SunnyDay on 2020/08/24
  */
 public class Trip {
-
     private class Node {
         private boolean isWord;// 除了叶子节点，中间的节点也可能为单词的结尾。
         private TreeMap<Character, Node> next;// 当前节点的下一节点。
