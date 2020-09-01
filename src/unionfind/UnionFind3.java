c5c498c6f9df9c2d8c57a7608fd93ff2d0e3af77
Administrator <1246771571@qq.com>
2020-09-01 21:05:45 +0800

    UnionFind 第三版 基于size的优化，优化树的深度降低 求并集时间复杂度

--- src/unionfind/UnionFind3.java
+++ src/unionfind/UnionFind3.java
@@ -2,17 +2,21 @@ package unionfind;
 
 /**
  * Create by SunnyDay on 2020/08/31
- * todo 第三版本的并查集：基于size的优化，减少树的深度。
- *
+ * 第三版本的并查集：基于size的优化，减少树的深度。
  */
 public class UnionFind3 implements UF {
 
     private int[] parent;
 
+    private int[] sz; //sz[i] 表示以i为根的集合中元素的个数
+
     public UnionFind3(int size) {
         parent = new int[size];
+        sz = new int[size];
+
         for (int i = 0; i < parent.length; i++) {
             parent[i] = i;
+            sz[i] = 1;// 初始时根节点就是自身，默认所有元素都处于不同集合，集合只有1个元素。
         }
     }
 
@@ -49,7 +53,15 @@ public class UnionFind3 implements UF {
         if (pRoot == qRoot) {
             return;
         }
-        parent[pRoot] = qRoot;
+
+        if (sz[pRoot] < sz[qRoot]) {
+            parent[pRoot] = qRoot;  // 将元素个数少的添加到元素个数多的上
+            sz[qRoot] += pRoot;// 合并元素个数
+        } else {
+            parent[qRoot] = pRoot;
+            sz[pRoot] += qRoot;
+        }
+
     }
 
     /**
